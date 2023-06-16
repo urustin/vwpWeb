1255e766969a4f8266b1721082ab5c3cdc96423f
Eric Son <urustin@naver.com>
2023-06-16 16:18:13 +1000

    update4

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import ProjectTab from '../components/project/ProjectTab';
 import ProjectList from '../components/project/ProjectList'; // 프로젝트 목록 컴포넌트
-import ProjectWhat from '../components/project/projectWhat';
+import ProjectWhat from '../components/project/ProjectWhat';
 import ProjectReview from '../components/project/ProjectReview';
 import Footer from '../components/Footer';
 
